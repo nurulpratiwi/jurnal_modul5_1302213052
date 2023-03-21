4daef31363780274eeb70b5475c4f881e599ad24
IFLAB <121171749+nurulpratiwi@users.noreply.github.com>
2023-03-21 16:48:37 +0700

    menambahkan class SimpleDataBase

--- jurnal_modul5_1302213052/Program.cs
+++ jurnal_modul5_1302213052/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+  
+    }
+
+    public void AddNewData( T data)
+    {
+     
+        this.storedData.Add(data);
+        this.inputDates.Add(DateTime.Now);
+        
+    }
+
+    public void PrintAllData()
+    { 
+        for(int i =0; i<storedData.Count(); i++)
+            {
+            Console.WriteLine("Data " + i + " berisi: " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+
+            }
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<float> DataBase = new SimpleDataBase<float>();
+        DataBase.AddNewData(13);
+        DataBase.AddNewData(02);
+        DataBase.AddNewData(21);
+        DataBase.PrintAllData();
+    }
+}
